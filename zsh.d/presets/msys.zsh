e14e88a9f5548d2219298f2d04345e595fa2bc88
Amane <Tanikaze_Amane@outlook.com>
2024-08-24 15:03:18 +0800

    feat(zsh.d): add fake-files completion for drives

--- zsh.d/presets/msys.zsh
+++ zsh.d/presets/msys.zsh
@@ -2,3 +2,7 @@ alias upgrade="pacman -Syu --noconfirm && pacman -Qdtq | xargs -r pacman -Rns --
 alias autoremove="pacman -Qdtq | xargs -r pacman -Rns --noconfirm"
 alias open="explorer"
 alias cls="clear && ff"
+
+drives=$(mount | sed -rn 's#^[A-Z]: on /([a-z]).*#\1#p' | tr '\n' ' ')
+zstyle ':completion:*' fake-files /: "/:$drives"
+unset drives
