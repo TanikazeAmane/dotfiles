f636a5e30939307fa6492b863a24b40bdaec7559
Amane <Tanikaze_Amane@outlook.com>
2024-10-22 10:05:50 +0800

    fix(msys): open

--- zsh.d/presets/msys.zsh
+++ zsh.d/presets/msys.zsh
@@ -1,8 +1,21 @@
 alias upgrade="pacman -Syu --noconfirm && pacman -Qdtq | xargs -r pacman -Rns --noconfirm"
 alias autoremove="pacman -Qdtq | xargs -r pacman -Rns --noconfirm"
-alias open="explorer"
+alias open="explorer_open"
 alias cls="clear && ff"
 
 drives=$(mount | sed -rn 's#^[A-Z]: on /([a-z]).*#\1#p' | tr '\n' ' ')
 zstyle ':completion:*' fake-files /: "/:$drives"
 unset drives
+
+explorer_open() {
+  if [ -z "$1" ]; then
+    explorer .
+  else
+    path=$(cygpath -w "$1")
+    if [ -d "$1" ]; then
+      explorer "$path"
+    else
+      start "" "$path"
+    fi
+  fi
+}
