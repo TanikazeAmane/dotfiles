e7e22e4d7cd401d4d3b7133f1966b5d56f672b38
Amane <Tanikaze_Amane@outlook.com>
2025-01-20 14:42:11 +0800

    fix(miniforge): too slow

--- zsh.d/presets/miniforge.zsh
+++ zsh.d/presets/miniforge.zsh
@@ -6,28 +6,15 @@ if [ -n "$CONDA_PATH" ]; then
 
             # >>> conda initialize >>>
             # !! Contents within this block are managed by 'conda init' !!
-            source "$CONDA_PATH/etc/profile.d/mamba.sh"
-            eval "$(${CONDA_PATH}/Scripts/conda.exe 'shell.zsh' 'hook' | sed -e 's/"$CONDA_EXE" $_CE_M $_CE_CONDA "$@"/"$CONDA_EXE" $_CE_M $_CE_CONDA "$@" | tr -d \x27\\r\x27/g')"
+            eval "$(cat "$CONDA_PATH/etc/profile.d/mamba.sh" | sed -e 's/"$CONDA_EXE" $_CE_M $_CE_CONDA "$@"/"$CONDA_EXE" $_CE_M $_CE_CONDA "$@" | tr -d \x27\\r\x27/g')"
+            eval "$(cat "$CONDA_PATH/etc/profile.d/conda.sh" | sed -e 's/"$CONDA_EXE" $_CE_M $_CE_CONDA "$@"/"$CONDA_EXE" $_CE_M $_CE_CONDA "$@" | tr -d \x27\\r\x27/g')"
             # <<< conda initialize <<<
             ;;
         *)
             # >>> conda initialize >>>
             # !! Contents within this block are managed by 'conda init' !!
-            __conda_setup="$(${CONDA_PATH}/bin/conda 'shell.zsh' 'hook' 2> /dev/null)"
-            if [ $? -eq 0 ]; then
-                eval "$__conda_setup"
-            else
-                if [ -f "$CONDA_PATH/etc/profile.d/conda.sh" ]; then
-                    . "$CONDA_PATH/etc/profile.d/conda.sh"
-                else
-                    export PATH="$CONDA_PATH/bin:$PATH"
-                fi
-            fi
-            unset __conda_setup
-
-            if [ -f "$CONDA_PATH/etc/profile.d/mamba.sh" ]; then
-                . "$CONDA_PATH/etc/profile.d/mamba.sh"
-            fi
+            source "$CONDA_PATH/etc/profile.d/conda.sh"
+            source "$CONDA_PATH/etc/profile.d/mamba.sh"
             # <<< conda initialize <<<
             ;;
     esac
